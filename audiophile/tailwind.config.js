15e14d5beef4b206dd228c01a70792c490c4c60b
lawrence1986 <madusquare@gmail.com>
2025-11-02 00:34:37 +0100

    more dependencies for project structure

---
+++ audiophile/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+content: ['./app/**/*.{ts,tsx,js,jsx}', './components/**/*.{ts,tsx,js,jsx}', './pages/**/*.{ts,tsx}'],
+theme: {
+extend: {
+colors: {
+brand: '#000000',
+accent: '#ffa111'
+},
+fontFamily: {
+sans: ['Inter', 'sans-serif']
+}
+}
+},
+plugins: [],
+};
